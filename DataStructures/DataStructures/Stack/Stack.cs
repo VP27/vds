658dd1589d5c61f20dbfe52bfc826a27202a69b2
VP <vivek.pv27@hotmail.com>
2016-05-02 14:08:00 +0530

    added stack class and its operations

---
+++ DataStructures/DataStructures/Stack/Stack.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Stack
+{
+    public class Stack
+    {
+        public void Push(int element)
+        {
+            return;
+        }
+
+        public int Pop()
+        {
+            return 1;
+        }
+
+        public int Size()
+        {
+            return 1;
+        }
+    }
+}
