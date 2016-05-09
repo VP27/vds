690814fbfdf92f077d56e031284dd90e776cd20a
vp27 <vivek.pv27@gmail.com>
2016-05-09 11:15:29 +0530

    Added Balanced Paranthesis evaluation logic

---
+++ DataStructures/DataStructures/Problems/BalancedParanthesisEvaluator.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Problems
+{
+    public class BalancedParanthesisEvaluator
+    {
+        private Dictionary<char, char> paranthesisPairs;
+
+        public BalancedParanthesisEvaluator()
+        {
+            paranthesisPairs = new Dictionary<char, char> {
+                { '{', '}' }, { '[', ']' }, { '(', ')' }
+            };
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="expression">
+        /// e.g. []
+        /// [{abcd}]
+        /// </param>
+        /// <returns></returns>
+        public bool CheckBalanceParanthesis(string expression)
+        {
+            if (string.IsNullOrEmpty(expression))
+                return false;
+
+            if (expression.Length == 1)
+                return false;
+
+            Stack<char> charStack = new Stack<char>();
+            char[] chars = expression.ToCharArray();
+            
+            if(!(chars.Contains('{') || chars.Contains('[') || chars.Contains('(')))
+                return false;
+
+            foreach (char c in chars)
+            {
+                if (paranthesisPairs.Keys.Contains(c))
+                {
+                    charStack.Push(c);
+                }
+
+                if (charStack.Count > 0)
+                {
+                    var peekValue = charStack.Peek();
+
+                    if (paranthesisPairs.Keys.Contains(peekValue))
+                    {
+                        if (c == paranthesisPairs[peekValue])
+                        {
+                            charStack.Pop();
+                        }
+                    }
+                }
+            }
+
+            return charStack.Count == 0;
+        }
+    }
+}
--- DataStructures/DataStructures/Program.cs
+++ DataStructures/DataStructures/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataStructures.Problems;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,21 @@ namespace DataStructures
 {
     public class Program
     {
+        private static BalancedParanthesisEvaluator paranthesisEvaluator = new BalancedParanthesisEvaluator();
+
         public static void Main(string[] args)
         {
-            return;
+            // check Balanced Paranthesis expression
+            CheckBalancedParanthesis();
+            Console.ReadKey();
+        }
+
+        private static void CheckBalancedParanthesis()
+        {
+            var input = "]";
+            var result = paranthesisEvaluator.CheckBalanceParanthesis(input) ? "Yes, balanced expression" : "No, not a balanced expression";
+
+            Console.WriteLine(result);
         }
     }
 }
